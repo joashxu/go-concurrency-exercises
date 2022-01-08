7368e780d5a7a103578af45e636a5bf15cda1795
Joash Xu <joash.xu@gmail.com>
2022-01-08 07:02:14 +0700

    Solution for graceful sigint

--- 4-graceful-sigint/main.go
+++ 4-graceful-sigint/main.go
@@ -13,9 +13,36 @@
 
 package main
 
+import (
+	"os"
+	"os/signal"
+)
+
 func main() {
 	// Create a process
 	proc := MockProcess{}
+
+	go func() {
+		// Create a channel to receive os.Signal values.
+		// This channel should be buffered.
+		sig := make(chan os.Signal, 1)
+
+		// Register the given channel to receive notifications.
+		// We use os.Interrupt instead of syscall.SIGINT so 
+		// golang can handle the correct interrupt for the platform
+		// it is running on (OS Agnostic). 
+		signal.Notify(sig, os.Interrupt)
+
+		// Block until we receive a signal
+		<-sig
+
+		// Remove all signal handler.
+		// sig will no longer receive any signal
+		signal.Reset()
+
+		// Gracefully ask the process to kill itself.
+		proc.Stop()
+	}()
 
 	// Run the process (blocking)
 	proc.Run()
